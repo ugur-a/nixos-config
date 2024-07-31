e9a50be1f70d73ebbcd383146e30be5407bf4cf2
Ughur Alakbarov <ugur.alekperov@gmail.com>
2024-07-31 17:28:02 +0200

    refactor(home/neovim/telescope): create a function to map Telescope actions

--- user/dev/editors/neovim/plugins/telescope.nix
+++ user/dev/editors/neovim/plugins/telescope.nix
@@ -15,48 +15,26 @@
 
       # Map Telescope builtin actions
       #  See `:help telescope.builtin`
-      keymaps = {
-        "<leader>sh" = {
-          action = "help_tags";
-          options.desc = "[S]earch [H]elp";
+      keymaps =
+        let
+          # inspired by LazyVim.pick()
+          map_telescope_action = action: desc: {
+            inherit action;
+            options.desc = desc;
+          };
+        in
+        {
+          "<leader>sh" = (map_telescope_action "help_tags" "[S]earch [H]elp");
+          "<leader>sk" = (map_telescope_action "keymaps" "[S]earch [K]eymaps");
+          "<leader>sf" = (map_telescope_action "find_files" "[S]earch [F]iles");
+          "<leader>ss" = (map_telescope_action "builtin" "[S]earch [S]elect Telescope");
+          "<leader>sw" = (map_telescope_action "grep_string" "[S]earch current [W]ord");
+          "<leader>sg" = (map_telescope_action "live_grep" "[S]earch by [G]rep");
+          "<leader>sd" = (map_telescope_action "diagnostics" "[S]earch [D]iagnostics");
+          "<leader>sr" = (map_telescope_action "resume" "[S]earch [R]esume");
+          "<leader>s" = (map_telescope_action "oldfiles" "[S]earch Recent Files ('.' for repeat)");
+          "<leader><leader>" = (map_telescope_action "buffers" "[ ] Find existing buffers");
         };
-        "<leader>sk" = {
-          action = "keymaps";
-          options.desc = "[S]earch [K]eymaps";
-        };
-        "<leader>sf" = {
-          action = "find_files";
-          options.desc = "[S]earch [F]iles";
-        };
-        "<leader>ss" = {
-          action = "builtin";
-          options.desc = "[S]earch [S]elect Telescope";
-        };
-        "<leader>sw" = {
-          action = "grep_string";
-          options.desc = "[S]earch current [W]ord";
-        };
-        "<leader>sg" = {
-          action = "live_grep";
-          options.desc = "[S]earch by [G]rep";
-        };
-        "<leader>sd" = {
-          action = "diagnostics";
-          options.desc = "[S]earch [D]iagnostics";
-        };
-        "<leader>sr" = {
-          action = "resume";
-          options.desc = "[S]earch [R]esume";
-        };
-        "<leader>s" = {
-          action = "oldfiles";
-          options.desc = "[S]earch Recent Files ('.' for repeat)";
-        };
-        "<leader><leader>" = {
-          action = "buffers";
-          options.desc = "[ ] Find existing buffers";
-        };
-      };
       settings = {
         extensions.__raw = "{ ['ui-select'] = { require('telescope.themes').get_dropdown() } }";
       };
@@ -99,7 +77,7 @@
       # Shortcut for searching your Neovim configuration files
       {
         mode = "n";
-        key = "<leader>sn";
+        key = "<leader>sc";
         action.__raw = ''
           function()
             require('telescope.builtin').find_files {
@@ -107,7 +85,58 @@
             }
           end
         '';
-        options.desc = "[S]earch [N]eovim files";
+        options.desc = "[S]earch [C]onfig";
+      }
+      # 
+      {
+        mode = "n";
+        key = "<leader>fd";
+        action.raw = ''
+          function()
+            require("telescope.builtin").find_files {
+              cwd = "~/.dotfiles"
+            }
+          end
+        '';
+        options.desc = "[F]ind [D]otfiles";
+      }
+      {
+        mode = "n";
+        key = "<leader>sd";
+        action.__raw = ''
+          function()
+            require("telescope.builtin").live_grep {
+              cwd = "~/.dotfiles"
+            }
+          end
+        '';
+        options.desc = "Grep Dotfiles";
+      }
+      # Prevent builtin colors from being displayed in the picker
+      {
+        mode = "n";
+        key = "<leader>uc";
+        action.__raw = ''
+          function()
+            -- stylua: ignore
+            local builtins = { "zellner", "torte", "slate", "shine", "ron", "quiet", "peachpuff",
+              "pablo", "murphy", "lunaperche", "koehler", "industry", "evening", "elflord",
+              "desert", "delek", "default", "darkblue", "blue", "morning"}
+
+            local target = vim.fn.getcompletion
+
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.fn.getcompletion = function()
+              return vim.tbl_filter(function(color)
+                return not vim.tbl_contains(builtins, color)
+              end, target("", "color"))
+            end
+
+            LazyVim.pick("colorscheme", { enable_preview = true })()
+            vim.fn.getcompletion = target
+          end,
+        '';
+        options.desc = "Colorscheme with Preview";
       }
     ];
   };
